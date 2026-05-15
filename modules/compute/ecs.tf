561dbaaa5fb2e44b2472a6d9c4adfb1a9666912d
aymenelk01 <aymenelkharchi15@gmail.com>
2026-05-15 14:23:38 +0200

    feat: update ECS task definition to use correct IAM role ARNs for execution and task roles

--- modules/compute/ecs.tf
+++ modules/compute/ecs.tf
@@ -29,7 +29,8 @@ resource "aws_ecs_task_definition" "task_definition" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions = jsonencode([
     {
       name      = "massar-app"
