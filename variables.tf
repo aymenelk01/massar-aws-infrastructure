b3e09a983b1a41b6efe4760cd4921a62efbd2e24
aymenelk01 <aymenelkharchi15@gmail.com>
2026-05-15 12:09:58 +0200

    feat: add variables for S3 bucket names for documents, state files, and logs

--- variables.tf
+++ variables.tf
@@ -22,3 +22,22 @@ variable "db_password" {
     type = string
     sensitive = true
 }
+
+# variable of the bucket name
+variable "documents_bucket_name" {
+  description = "The name of the S3 bucket to create for documents"
+  default = "documents_bucket_${var.environment}"
+  type        = string
+}
+
+# variable for the bucket name for state files
+variable "state_bucket_name" {
+  description = "The name of the S3 bucket to create for state files"
+  type        = string
+}
+
+# variable for the log bucket name
+variable "logs_bucket_name" {
+  description = "The name of the S3 bucket to create for log files"
+  type        = string
+}
