0795f30444541300b191b1aa3cee920ca0d1bab1
aymenelk01 <aymenelkharchi15@gmail.com>
2026-05-15 14:39:23 +0200

    feat: add us-east-1 region for WAF

--- providers.tf
+++ providers.tf
@@ -7,6 +7,13 @@ terraform {
   }
 }
 
+# Default provider — eu-south-1
 provider "aws" {
   region = var.aws_region
+}
+
+# Second provider — us-east-1 for WAF and ACM (CloudFront requirements)
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
 }
