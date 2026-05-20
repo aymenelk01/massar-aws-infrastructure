9dbed949b22e899b5f63a1cb13cc50e2b172673c
aymenelk01 <aymenelkharchi15@gmail.com>
2026-05-20 17:07:10 +0200

    feat: add SQS queue ARN output for notifications module

--- modules/notifications/outputs.tf
+++ modules/notifications/outputs.tf
@@ -2,6 +2,11 @@ output "sqs_queue_url" {
     value       = aws_sqs_queue.main.url
     description = "URL of the SQS queue for notifications"
 }
+
+output "sqs_queue_arn" {
+    value       = aws_sqs_queue.main.arn
+    description = "ARN of the SQS queue for notifications"
+}
  
 output "sns_topic_arn" {
     value       = aws_sns_topic.notifications_topic.arn
