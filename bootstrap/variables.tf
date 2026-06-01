11e8c0b26b47b247d602595ea2ead7db0085516f
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-01 23:35:35 +0200

    style: adjust variable formatting for consistency in variables.tf

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -1,37 +1,42 @@
 variable "environment" {
-    description = "The environment for which to create the resources (e.g., dev, staging, prod)"
-    type        = string
+  description = "The environment for which to create the resources (e.g., dev, staging, prod)"
+  type        = string
 }
 
 variable "aws_region" {
-    description = "The AWS region where the resources will be created"
-    type        = string
+  description = "The AWS region where the resources will be created"
+  type        = string
 }
 
 variable "oidc_terraform_role_name" {
-    description = "The name of the IAM role to create for GitHub Actions OIDC authentication"
-    type        = string
+  description = "The name of the IAM role to create for GitHub Actions OIDC authentication"
+  type        = string
 }
 
 variable "oidc_deploy_role_name" {
-    description = "The name of the IAM role to create for GitHub Actions OIDC authentication"
-    type        = string
+  description = "The name of the IAM role to create for GitHub Actions OIDC authentication"
+  type        = string
 }
 
 variable "github_repo_name" {
-    description = "The name of the GitHub repository (e.g., aymenelk01/massar-aws-infrastructure)"
-    type        = string
+  description = "The name of the GitHub repository (e.g., aymenelk01/massar-aws-infrastructure)"
+  type        = string
 }
 
 variable "github_branch_name" {
-    description = "The name of the GitHub branch (e.g., main)"
-    type        = string
-    default     = "main"
+  description = "The name of the GitHub branch (e.g., main)"
+  type        = string
+  default     = "main"
 }
 
 variable "github_username" {
-    description = "The GitHub username for the repository (e.g., aymenelk01)"
-    type        = string
+  description = "The GitHub username for the repository (e.g., aymenelk01)"
+  type        = string
+}
+
+variable "github_app_repo_name" {
+  description = "The name of the GitHub repository where the application code is stored (e.g., aymenelk01/massar-app)"
+  type        = string
 }
 
 variable "static_bucket_name" {
