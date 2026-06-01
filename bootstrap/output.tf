dfb36b20bff797d47a664c0184e173208bce8350
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-01 23:34:51 +0200

    style: format output variables for consistency

--- bootstrap/output.tf
+++ bootstrap/output.tf
@@ -1,9 +1,9 @@
 output "deploy_role_arn" {
-    value = aws_iam_role.deploy_role.arn
-    description = "The ARN of the IAM role for deploy OIDC authentication"
+  value       = aws_iam_role.deploy_role.arn
+  description = "The ARN of the IAM role for deploy OIDC authentication"
 }
 
 output "terraform_role_arn" {
-    value = aws_iam_role.terraform_role.arn
-    description = "The ARN of the IAM role for Terraform OIDC authentication"
+  value       = aws_iam_role.terraform_role.arn
+  description = "The ARN of the IAM role for Terraform OIDC authentication"
 }
