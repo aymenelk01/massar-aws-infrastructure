45330bd5f22eb27d3197e5eb7bc5bbcb9c7f47c3
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-08 17:34:17 +0200

    feat : remove the unsed dynamodb gateway

--- infra/modules/vpc/gatewayendpoints.tf
+++ infra/modules/vpc/gatewayendpoints.tf
@@ -15,22 +15,3 @@ resource "aws_vpc_endpoint" "s3_gateway" {
         Environment = var.environment
 }
 }
-
-# create an vpc endpoint for dynamodb
-resource "aws_vpc_endpoint" "dynamodb_gateway" {
-    vpc_id = aws_vpc.main.id
-    service_name = "com.amazonaws.${var.aws_region}.dynamodb"
-    vpc_endpoint_type = "Gateway"
-    
-    # associate the endpoint with all route tables in the VPC
-    route_table_ids = concat(
-        [aws_route_table.public_RT.id],
-        aws_route_table.app_RT[*].id
-    )
-
-    tags = {
-        Name = "DynamoDBGatewayEndpoint-${var.environment}"
-        Environment = var.environment
-}
-}
-
