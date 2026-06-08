67b67fc14d5fbdedcca668ef6fc1751123591694
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-08 17:01:21 +0200

    feat: add Checkov skip reasons for Secrets Manager secret configuration

--- infra/modules/database/secrets.tf
+++ infra/modules/database/secrets.tf
@@ -1,5 +1,7 @@
 # Create a Secrets Manager secret to store the database credentials
 resource "aws_secretsmanager_secret" "credential_secret" {
+  # checkov:skip=CKV2_AWS_57: Portfolio project- not enabling rotation for the database credentials secret to avoid additional complexity and costs associated with Lambda functions for rotation, which is acceptable for a portfolio project.
+  # checkov:skip=CKV_AWS_149: Portfolio project- AWS default encryption is sufficient for a portfolio project, so using the default AWS-managed encryption to avoid additional costs from a custom KMS key.
     name = "db-credentials-${var.environment}"
     description = "Secret for RDS database credentials for ${var.environment} environment"
     
