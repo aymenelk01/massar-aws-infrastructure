5929d000ef7fdf7e9040b5dc00e291f5b6a344e9
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-08 16:53:37 +0200

    feat: skip some checkov violation with add the reasons

--- infra/modules/compute/cloudwatch.tf
+++ infra/modules/compute/cloudwatch.tf
@@ -1,6 +1,8 @@
 resource "aws_cloudwatch_log_group" "ecs_log_group" {
+
+  # checkov:skip=CKV_AWS_158:Portfolio project- AWS managed encryption acceptable
   name              = "/ecs/massar-${var.environment}"
-  retention_in_days = 30
+  retention_in_days = 365
 
   tags = {
     Name        = "ECSLogGroup-${var.environment}"
@@ -9,8 +11,10 @@ resource "aws_cloudwatch_log_group" "ecs_log_group" {
 }
 
 resource "aws_cloudwatch_log_group" "ecs_exec_log_group" {
+  # checkov:skip=CKV_AWS_158:Portfolio project- AWS managed encryption acceptable
+
   name              = "/ecs/exec/massar-${var.environment}"
-  retention_in_days = 30
+  retention_in_days = 7
 
   tags = {
     Name        = "ECSExecLogGroup-${var.environment}"
