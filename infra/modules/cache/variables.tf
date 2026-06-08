2bfa43045c2299748e936b1d6c1c9b07ff468304
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-08 16:26:07 +0200

    fix: add availability_zones variable to cache module

--- infra/modules/cache/variables.tf
+++ infra/modules/cache/variables.tf
@@ -14,3 +14,9 @@ variable "private_db_subnet_ids" {
   description = "List of private database subnet IDs"
   type        = list(string)
 }
+
+variable "availability_zones" {
+  description = "List of availability zones for the ElastiCache cluster"
+  type        = list(string)
+  default     = ["eu-south-1a", "eu-south-1b"]
+}
