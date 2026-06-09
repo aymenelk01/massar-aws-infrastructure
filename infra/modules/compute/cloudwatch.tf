ada14dabc85c3002aaf6ee655970273e234674fe
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-09 15:00:40 +0200

    feat: change the retention day to 7 for reducing the costs

--- infra/modules/compute/cloudwatch.tf
+++ infra/modules/compute/cloudwatch.tf
@@ -1,8 +1,8 @@
 resource "aws_cloudwatch_log_group" "ecs_log_group" {
-
+  # checkov:skip=CKV_AWS_338:Portfolio project optimized for cost insulation. Retaining logs for 1 year exceeds free tier limits.
   # checkov:skip=CKV_AWS_158:Portfolio project- AWS managed encryption acceptable
   name              = "/ecs/massar-${var.environment}"
-  retention_in_days = 365
+  retention_in_days = 7
 
   tags = {
     Name        = "ECSLogGroup-${var.environment}"
@@ -11,6 +11,7 @@ resource "aws_cloudwatch_log_group" "ecs_log_group" {
 }
 
 resource "aws_cloudwatch_log_group" "ecs_exec_log_group" {
+  # checkov:skip=CKV_AWS_338:Portfolio project optimized for cost insulation. Retaining logs for 1 year exceeds free tier limits.
   # checkov:skip=CKV_AWS_158:Portfolio project- AWS managed encryption acceptable
 
   name              = "/ecs/exec/massar-${var.environment}"
