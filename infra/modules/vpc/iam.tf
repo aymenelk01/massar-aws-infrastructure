1fc8e8b3e0a99226b2da4a33f2e7636c5d1df244
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-09 17:57:33 +0200

    feat: update VPC flow log IAM policy resource and define flow log resource with proper configuration

--- infra/modules/vpc/iam.tf
+++ infra/modules/vpc/iam.tf
@@ -46,7 +46,7 @@ resource "aws_iam_role_policy" "vpcflow_policy" {
         Action = [
           "logs:DescribeLogStreams"
         ]
-        Resource = "arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:*"
+        Resource = "arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:*:*"
       },
 
       {
