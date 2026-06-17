56559dc6fad0f251580ffe4144216b6e07011c00
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-17 11:02:57 +0200

    style: format security module parameters for consistency

--- infra/main.tf
+++ infra/main.tf
@@ -7,10 +7,10 @@ module "vpc" {
 
 # call the security module and pass the vpc id from the vpc module
 module "security" {
-  source                    = "./modules/security"
-  environment               = var.environment
-  vpc_id                    = module.vpc.vpc_id
-  vpc_cidr_block            = module.vpc.vpc_cidr_block
+  source         = "./modules/security"
+  environment    = var.environment
+  vpc_id         = module.vpc.vpc_id
+  vpc_cidr_block = module.vpc.vpc_cidr_block
 }
 
 module "endpoints" {
