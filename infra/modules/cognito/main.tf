fffb247d06cc4cb96c730eff2bfeeaf690f9186c
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-18 13:46:12 +0200

    n/a

--- infra/modules/cognito/main.tf
+++ infra/modules/cognito/main.tf
@@ -29,7 +29,7 @@ resource "aws_cognito_user_pool" "massar_pool" {
 
 # Create a user pool client for the Cognito user pool to allow the application to interact with the user pool
 resource "aws_cognito_user_pool_client" "massar_client" {
-  name         = "massar-client-${var.environment}"
+  name         = "massar-users-client-${var.environment}"
   user_pool_id = aws_cognito_user_pool.massar_pool.id
 
   explicit_auth_flows = [
@@ -53,3 +53,4 @@ resource "aws_cognito_user_group" "teachers" {
   name         = "teachers"
   user_pool_id = aws_cognito_user_pool.massar_pool.id
 }
+
