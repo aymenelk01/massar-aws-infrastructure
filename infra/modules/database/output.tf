ce950e904cf8d2259f48f8c7536924cabf2bb087
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-19 18:06:22 +0200

    fix: update database output variables and remove deprecated secret references

--- infra/modules/database/output.tf
+++ infra/modules/database/output.tf
@@ -3,22 +3,22 @@ output "rds_proxy_endpoint" {
   value       = aws_db_proxy.proxy.endpoint
 }
 
+output "rds_proxy_resource_id" {
+  value       = aws_db_proxy.proxy.id
+  description = "The resource ID of the RDS Proxy"
+  }
+
 output "aurora_cluster_endpoint" {
   description = "The endpoint of the Aurora cluster"
   value       = aws_rds_cluster.aurora.endpoint
 }
   
-output "db_secret_arn" {
-  description = "The ARN of the database secret"
-  value       = aws_secretsmanager_secret.credential_secret.arn
-}
-
 output "db_name" {
   description = "the name of the database"
   value       = aws_rds_cluster.aurora.database_name
 }
 
-output "secretmanager_arn" {
-  description = "the ARN of the secret manager"
-  value       = aws_secretsmanager_secret.credential_secret.arn
+output "db_password_secret_arn" {
+  value       = aws_rds_cluster.aurora.master_user_secret[0].secret_arn
+  description = "The ARN of the AWS-managed master password secret"
 }
