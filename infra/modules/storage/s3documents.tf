122a5c21692d254b7740dab73de86bdd9d65fab1
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-22 17:07:43 +0200

    feat: add non-current version expiration configuration to S3 bucket lifecycle

--- infra/modules/storage/s3documents.tf
+++ infra/modules/storage/s3documents.tf
@@ -70,6 +70,12 @@ resource "aws_s3_bucket_lifecycle_configuration" "documents_files_lifecycle" {
       days          = 365
       storage_class = "GLACIER"
     }
+
+    # Specifies configuration to expire non-current object versions
+    noncurrent_version_expiration {
+      noncurrent_days           = 30  # Expire non-current versions after 30 days
+      newer_noncurrent_versions = 3   # Keep the 3 most recent non-current versions
+    }
   }
 }
 
