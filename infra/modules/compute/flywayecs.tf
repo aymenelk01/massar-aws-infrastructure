35cd52f8391f9927a5f978f2ed1a4fbdeb7d9ada
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-22 14:03:34 +0200

    feat: migrate ECS task flyway to Graviton for better performance and less costs

--- infra/modules/compute/flywayecs.tf
+++ infra/modules/compute/flywayecs.tf
@@ -7,6 +7,11 @@ resource "aws_ecs_task_definition" "flyway" {
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.flyway_execution_role.arn
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "ARM64"
+  }
+  
 
   container_definitions = jsonencode([
     {
